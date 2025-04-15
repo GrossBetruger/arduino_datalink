fcbf9fb386cfd1f97c466e13b267a6510d9bd1f1
GrossBetruger <koriatoren@gmail.com>
2025-04-15 23:29:45 +0300

    feat: python serial reader

---
+++ serial_reader.py
@@ -0,0 +1,29 @@
+import os
+import serial
+from time import sleep
+# Replace 'COM3' with your Arduino’s serial port (e.g., '/dev/ttyUSB0' for Linux)
+macos_port = '/dev/tty.usbmodem2101'
+linux_port = '/dev/ttyUSB0'
+
+try:
+    ser = serial.Serial(macos_port, 9600, timeout=1)
+except serial.SerialException:  
+    print(f"Serial port not found: {macos_port}")
+    # run ls /dev/tty.* to find the correct port and print the output
+    print("Available ports:")
+    print(os.popen('ls /dev/tty.*').read())
+    exit()
+
+# Open (or create) a file to save the serial data
+with open('serial_log.txt', 'w') as f:
+    try:
+        while True:
+            ser.write(bytes(input("Enter a message: "), 'utf-8'))
+            sleep(0.1)
+            while (serial_data := ser.read()) != b'\n':
+                print(serial_data)
+                
+
+    except KeyboardInterrupt:
+        print("Data capture stopped.")
+
